e2b448a9bb786c2e25479667d83ffd6dfa9edbd4
willians <williamsnieves@gmail.com>
2015-08-05 17:54:46 -0430

    adapter created

---
+++ app/src/main/java/com/example/willians/movieapp/ui/adapters/MovieListAdapter.java
@@ -0,0 +1,86 @@
+package com.example.willians.movieapp.ui.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.willians.movieapp.R;
+import com.example.willians.movieapp.domains.Movie;
+
+import java.util.ArrayList;
+
+/**
+ * Created by willians on 05/08/15.
+ */
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieListViewHolder> {
+
+    ArrayList<Movie> movies;
+    Context context;
+
+    public MovieListAdapter(Context context){
+        this.context = context;
+        this.movies = new ArrayList<>();
+
+    }
+
+    @Override
+    public MovieListAdapter.MovieListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_movie_list, parent, false);
+
+        return new MovieListViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieListAdapter.MovieListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public void addAll(ArrayList<Movie> movies){
+        if(movies == null)
+            throw new NullPointerException("The items cannot be null");
+
+        this.movies.addAll(movies);
+        notifyDataSetChanged();
+
+
+    }
+
+
+    public class MovieListViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imgUrl;
+        TextView movieName;
+        TextView movieDesc;
+        TextView movieAvg;
+
+        public MovieListViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void setMovieImage(String url){
+            //imgUrl.src(url);
+            //picasso
+        }
+
+        public void setMovieName(String name){
+            movieName.setText(name);
+        }
+
+        public void setMovieDesc(String description){
+            movieDesc.setText(description);
+        }
+
+        public void setMovieAverage(String average){
+            movieAvg.setText(average);
+        }
+    }
+}
